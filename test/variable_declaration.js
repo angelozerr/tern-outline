37c63b78fcc41fc90ec23f8286ebde51754d0d3e
angelozerr <angelo.zerr@gmail.com>
2015-08-08 17:03:24 +0200

    Fix test

--- test/variable_declaration.js
+++ test/variable_declaration.js
@@ -3,8 +3,8 @@ var util = require("./util");
 exports['test simple variable declaration'] = function() {
   util.assertOutline("var a, b ", {"outline":
     [
-     {"name":"a","type":"VariableDeclaration","start":4,"end":5},
-     {"name":"b","type":"VariableDeclaration","start":7,"end":8}
+     {"name":"a","type":"?","start":4,"end":5},
+     {"name":"b","type":"?","start":7,"end":8}
    ]
   });    
 }
@@ -12,8 +12,18 @@ exports['test simple variable declaration'] = function() {
 exports['test unkown variable declaration'] = function() {
   util.assertOutline("var a, b, ", {"outline":
     [
-     {"name":"a","type":"VariableDeclaration","start":4,"end":5},
-     {"name":"b","type":"VariableDeclaration","start":7,"end":8}
+     {"name":"a","type":"?","start":4,"end":5},
+     {"name":"b","type":"?","start":7,"end":8}
+   ]
+  });    
+}
+
+exports['test variable declaration with simple type'] = function() {
+  util.assertOutline("var a=1, b='', c = [true], ", {"outline":
+    [
+     {"name":"a","type":"number","start":4,"end":5},
+     {"name":"b","type":"string","start":9,"end":10},
+     {"name":"c","type":"[bool]","start":15,"end":16}
    ]
   });    
 }
